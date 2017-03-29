bd093576835c8e75c8ab085440ea1d642dabcdea
kuuurt13 <kstac13@gmail.com>
2017-03-28 21:31:32 -0400

    task(ci): Update karma to use chrome for testing

--- karma.conf.js
+++ karma.conf.js
@@ -3,15 +3,12 @@
 module.exports = config => {
   config.set({
     autoWatch: true,
-    browsers: ['PhantomJS'],
+    browsers: ['Chrome'],
     files: [
       'karma.shim.js'
     ],
     frameworks: ['jasmine'],
     logLevel: config.LOG_INFO,
-    phantomJsLauncher: {
-      exitOnResourceError: true
-    },
     port: 9876,
     preprocessors: {
       'karma.shim.js': ['webpack', 'sourcemap']
@@ -21,6 +18,12 @@ module.exports = config => {
     webpack: require('./webpack.config.js'),
     webpackServer: {
       noInfo: true
+    },
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
     }
   });
 };
