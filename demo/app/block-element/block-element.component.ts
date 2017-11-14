ee9e2ea9ab108492c1b4d3d61d1b7e0e6abbcbe8
kuuurt13 <kstac13@gmail.com>
2017-11-13 20:21:50 -0500

    dev(demo): use systemjs to dynamically test load lib changes (#41)

--- demo/app/block-element/block-element.component.ts
+++ demo/app/block-element/block-element.component.ts
@@ -4,7 +4,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'block-element',
   templateUrl: './block-element.component.html',
-  styleUrls: ['./block-element.component.css']
+  styleUrls: ['./block-element.component.css'],
+  moduleId: __moduleName
 })
 export class BlockElementComponent {
 
--- demo/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { enableProdMode } from '@angular/core';
-import { AppModule } from './app/app.module';
-require('./styles.css');
-
-platformBrowserDynamic().bootstrapModule(AppModule);
