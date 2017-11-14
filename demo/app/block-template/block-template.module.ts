521acbfd80cf41f04a2ee3c5012a250a380314b3
kuuurt13 <kstac13@gmail.com>
2017-11-13 20:21:50 -0500

    dev(demo): use systemjs to dynamically test load lib changes (#41)

---
+++ demo/app/block-template/block-template.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BlockTemplateComponent } from './block-template.component';
+
+@NgModule({
+  declarations: [
+    BlockTemplateComponent
+  ],
+  exports: [
+    BlockTemplateComponent
+  ],
+  entryComponents: [
+    BlockTemplateComponent
+  ]
+})
+export class BlockTemplateModule { }
