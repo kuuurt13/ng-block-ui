b3e228c18fdd719d8237284a1d4598a062982a81
kuuurt13 <kstac13@gmail.com>
2017-11-13 21:36:06 -0500

    feat(blockUI): add isActive boolean to blockUI instance (#40)

--- lib/services/block-ui-instance.service.ts
+++ lib/services/block-ui-instance.service.ts
@@ -5,6 +5,7 @@ import { BlockUIActions } from '../constants/block-ui-actions.constant';
 import { BlockUIDefaultName } from '../constants/block-ui-default-name.constant';
 import { NgBlockUI } from '../models/block-ui.model';
 import { BlockUISettings } from '../models/block-ui-settings.model';
+import { BlockUIEvent } from '../models/block-ui-event.model';
 import { BlockUIService } from '../../index';
 
 
@@ -13,8 +14,11 @@ export class BlockUIInstanceService {
   blockUISettings: BlockUISettings | any = {};
   private blockUISubject: ReplaySubject<any> = new ReplaySubject();
   private blockUIObservable: Observable<any> = this.blockUISubject.asObservable();
+  private instances: NgBlockUI[] = [];
 
-  constructor() { }
+  constructor() {
+    this.blockUIObservable.subscribe(this.blockUIMiddleware.bind(this));
+  }
 
   getSettings(): BlockUISettings | any {
     return this.blockUISettings;
@@ -25,17 +29,44 @@ export class BlockUIInstanceService {
   }
 
   decorate(name: string = BlockUIDefaultName): NgBlockUI {
-    return {
+    const blockUI = {
+      name,
+      isActive: false,
       start: this.dispatch(this.blockUISubject, BlockUIActions.START, name),
       update: this.dispatch(this.blockUISubject, BlockUIActions.UPDATE, name),
       stop: this.dispatch(this.blockUISubject, BlockUIActions.STOP, name),
       reset: this.dispatch(this.blockUISubject, BlockUIActions.RESET, name),
       unsubscribe: this.dispatch(this.blockUISubject, BlockUIActions.UNSUBSCRIBE, name)
     } as NgBlockUI;
+
+    this.instances.push(blockUI);
+
+    return blockUI;
   }
 
   observe(): Observable<any> {
     return this.blockUIObservable;
+  }
+
+  private blockUIMiddleware({ action, name }: BlockUIEvent): void {
+    let isActive = null;
+
+    switch (action) {
+      case (BlockUIActions.START):
+        isActive = true;
+        break;
+
+      case (BlockUIActions.STOP):
+      case (BlockUIActions.RESET):
+        isActive = false;
+        break;
+    }
+
+    if (isActive !== null) {
+      this.instances.forEach(i =>
+        i.isActive = i.name === name ? isActive : i.isActive
+      );
+    }
   }
 
   private dispatch(subject: ReplaySubject<any>, action: BlockUIActions, name: string = BlockUIDefaultName): Function {
