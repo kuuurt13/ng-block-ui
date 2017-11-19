5a131ac330786703fa6834ba5cbf39d7cc24fa4e
kuuurt13 <kstac13@gmail.com>
2017-11-19 10:54:51 -0500

    feat(router): prevent navigation when blocking (#39)

---
+++ router.ts
@@ -0,0 +1,7 @@
+import { BlockUIRouterModule } from './lib/router/block-ui-router.module';
+import { BlockUIPreventNavigation } from './lib/router/block-ui-prevent-navigation.service';
+
+export {
+  BlockUIRouterModule,
+  BlockUIPreventNavigation
+};
