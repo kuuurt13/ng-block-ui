87d6682cddbf2eaf7383b1161b6c06409d140681
kuuurt13 <kstac13@gmail.com>
2018-03-14 21:40:20 -0400

    fix(http): make module AOT compatible

--- lib/http/block-ui-http.module.ts
+++ lib/http/block-ui-http.module.ts
@@ -1,23 +1,35 @@
-import { NgModule, ModuleWithProviders } from '@angular/core';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-
-import { BlockUIModule, BlockUIServiceInstance } from '../block-ui.module';
-import { BlockUIInterceptor } from './block-ui-http.service';
+import { NgModule, ModuleWithProviders, InjectionToken } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { BlockUIModule } from '../block-ui.module';
+import { BlockUIHttpSettings } from './block-ui-http-settings.service';
+import { BlockUIInterceptor } from './block-ui-http.interceptor';
 import { HttpSettings } from '../models/block-ui-http-settings.model';
 
 
+// Needed for AOT compiling
+export const BlockUIHttpModuleSettings = new InjectionToken<string>('BlockUIHttpModuleSettings');
+
+export function provideSettingsInstance(settings: HttpSettings): BlockUIHttpSettings {
+  return { settings };
+}
+
 @NgModule({
-  imports: [
-    BlockUIModule
-  ]
+  imports: [BlockUIModule]
 })
 export class BlockUIHttpModule {
   public static forRoot(settings: HttpSettings = {}): ModuleWithProviders {
-    BlockUIServiceInstance.updateSettings({ http: settings });
-
     return {
       ngModule: BlockUIHttpModule,
       providers: [
+        {
+          provide: BlockUIHttpModuleSettings,
+          useValue: settings
+        },
+        {
+          provide: BlockUIHttpSettings,
+          useFactory: provideSettingsInstance,
+          deps: [BlockUIHttpModuleSettings]
+        },
         {
           provide: HTTP_INTERCEPTORS,
           useClass: BlockUIInterceptor,
