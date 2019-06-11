9403d1af91853e75c24f7c50330e2cfcd32acd36
kuuurt13 <kstac13@gmail.com>
2019-06-11 18:20:04 -0400

    fix(BlockUIContentComponent): Fixed type error

--- lib/components/block-ui-content/block-ui-content.component.ts
+++ lib/components/block-ui-content/block-ui-content.component.ts
@@ -170,8 +170,8 @@ export class BlockUIContentComponent implements OnInit, AfterViewInit, AfterView
   private hideBlock() {
     this.clearState();
     this.active = false;
-    if (!( this.changeDetectionRef ).destroyed){
-        this.changeDetectionRef.detectChanges();
+    if (!this.changeDetectionRef['destroyed']) {
+      this.changeDetectionRef.detectChanges();
     }
   }
 
